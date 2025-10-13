2f1f618e3bf213b67fb0b9baa5d032b679e60789
yoann <yoanngervais.yg13750@gmail.com>
2025-10-13 12:29:05 +0200

    error

--- app/Models/Artiste.php
+++ app/Models/Artiste.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Artiste extends Model
 {
-    //
+    Schema::create('artistes', function (Blueprint $table) {
+        $table->id();
+        $table->string('profession');
+        $table->string('style');
+        $table->string('telephone')->unique();
+        $table->string('pseudo')->unique();
+        $table->string('adresse');
+        $table->string('instagram');
+        $table->string('facebook');
+        $table->string('image');
+        $table->timestamps();
+    });
 }
